ff58264906a066e0d47d21bec4b2b8f404e0580d
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-01-04 13:49:34 +0200

    Play, List style
    - simple play function
    - new list style
    - song toggle selection

--- app/js/common.js
+++ app/js/common.js
@@ -9,29 +9,47 @@ var Songs = [
 	{
 		artist: "Imagine Dragons",
 		name:"Dream",
-		src: "Imagine Dragons - Dream"
+		src: "Imagine Dragons – Dream"
 	},
 	{
 		artist: "Ben Howard",
 		name:"All Is Now Harmed",
-		src: "Imagine Dragons – Believer"
+		src: "Ben Howard – All Is Now Harmed"
 	},
 	{
 		artist: "Ben Howard",
 		name:"Oats In The Water",
-		src: "Ben Howard– Oats In The Water"
+		src: "Ben Howard – Oats In The Water"
 	},
 	{
 		artist: "Ben Howard",
 		name:"Promise",
-		src: "Ben Howard – Promise"	
+		src: "Ben Howard – Promise"
 	}
 
 ]
 
-app.controller("AudioPlayerCtrl", function ($scope) {
-
+app.controller("AudioPlayerCtrl", function ($scope, ) {
 	
+	const AudioFolder = "audio/";
+	const AudioFormat = ".mp3";
+
+	$("#player").attr('type', `audio/${AudioFormat.substring(1,1)}`);
+
+	$scope.CurrentSong;
+
+	$scope.Songs = Songs;
+
+	$scope.Play = function (index, src) {
+		console.log(AudioFolder+src+AudioFormat);
+
+		$("#player").attr('src', AudioFolder+src+AudioFormat);
+		$("#player")[0].play();	
+
+		$(`.song:eq(${$scope.CurrentSong})`).removeClass('selected');
+		$(`.song:eq(${index})`).addClass('selected');
+		$scope.CurrentSong = index;
+	}
 		
 	
 })
--- gulpfile.js
+++ gulpfile.js
@@ -27,8 +27,9 @@ gulp.task('common-js', function() {
 
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
+		"app/libs/jquery/dist/jquery.min.js",
 		"app/libs/angular/angular.min.js",
-		'app/js/common.min.js' // Always in the end
+		"app/js/common.min.js" // Always in the end
 		])
 	.pipe(concat('scripts.min.js'))
 	//.pipe(uglify()) // Comment optionaly
